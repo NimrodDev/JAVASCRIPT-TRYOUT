bb10019f3e70308873e176fc08f142ed1a261c59
Moriel Nimrod Yusuf <142363696+NimrodDev@users.noreply.github.com>
2023-10-28 14:31:53 +0100

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,12 @@
+// Declare a variable using var
+var name = "John";
+console.log(name); // Output: John
+
+// Declare a variable using let
+let age = 25;
+console.log(age); // Output: 25
+
+
+const city = "Abuja";
+console.log(Boolean(city)); // Output: true
+
